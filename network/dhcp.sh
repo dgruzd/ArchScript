f047928d8a069e873bf8b8a727fe4c59cde7af1b
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-02 13:44:43 +0400

    dhcp script

---
+++ network/dhcp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "interface=\"eth0\"" >> /etc/rc.local
+echo "address=" >> /etc/rc.local
+echo "netmask=" >> /etc/rc.local
+echo "gateway" >> /etc/rc.local
+
+/etc/rc.d/network restart
